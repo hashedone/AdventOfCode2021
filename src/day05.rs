4eab593537da9b68295978ce8ab7249f6460a803
Łukasz Piwowar <zsacul@gmail.com>
2021-12-05 19:48:52 +0100

    day05

--- src/day05.rs
+++ src/day05.rs
@@ -2,107 +2,75 @@ use std::collections::{HashMap};
 
 fn get_point(s:&String)->(i32,i32)
 {
-    let p :Vec<_>= s.split(',').collect();
-    (p[0].parse::<i32>().unwrap(),p[1].parse::<i32>().unwrap())
+    let p : Vec<_>= s.split(',').collect();
+    (p[0].parse::<i32>().unwrap() , p[1].parse::<i32>().unwrap())
 }
 
-pub fn part1(data:&Vec<String>)->i32
+fn draw(data:&Vec<String>,diagonal:bool)->i32
 {
     let mut field: HashMap<(i32,i32),i32> = HashMap::new();
 
     for l in data {
-        let v:Vec<&str> = l.split(" -> ").collect();
+        let  v: Vec<_>   = l.split(" -> ").collect();
         let p1 = get_point(&v[0].to_string());
         let p2 = get_point(&v[1].to_string());
 
-        println!("p1 {}x{}",p1.0,p1.1);
-        println!("p2 {}x{}",p2.0,p2.1);
+        let mut n = 1 + i32::max(i32::abs(p2.0-p1.0),i32::abs(p2.1-p1.1));
 
-
-        if p1.0==p2.0
-        {
-            for i in i32::min(p1.1,p2.1)..=i32::max(p1.1,p2.1) {
-                let p = (p1.0,i);
-                field.insert(p , field.get(&p).unwrap_or(&0)+1  );
-            }
+        let (dx,dy) =      
+        if p1.0==p2.0 
+        { 
+            if p1.1<p2.1 { ( 0, 1) } 
+                    else { ( 0,-1) } 
         }
-        else if p1.1==p2.1
+          else if p1.1==p2.1
         {
-            for i in i32::min(p1.0,p2.0)..=i32::max(p1.0,p2.0) {
-                let p = (i,p1.1);
-                field.insert(p , field.get(&p).unwrap_or(&0)+1  );
-                
-            }
+            if p1.0<p2.0 { ( 1, 0) } 
+                    else { (-1, 0) } 
         }
+          else
+        {
+            if diagonal 
+            {
+                if p1.0<p2.0 
+                { 
+                    if p1.1<p2.1 { ( 1, 1) } 
+                            else { ( 1,-1) }             
+                } 
+                  else 
+                { 
+                    if p1.1<p2.1 { (-1, 1) } 
+                            else { (-1,-1) }             
+                }             
+            }
+              else       
+            {             
+                n = 0;
+                ( 0, 0)
+            }
+        };
 
-        
+        let (mut x,mut y) = p1;
+        for _ in 0..n 
+        {
+            let point = (x,y);
+            field.insert(point, field.get(&point).unwrap_or(&0)+1 );            
+            x+=dx;
+            y+=dy;
+        }
     }
     field.values().filter(|&&v|v>1).count() as i32
+}
+
+
+pub fn part1(data:&Vec<String>)->i32
+{
+    draw(data,false)
 }
 
 pub fn part2(data:&Vec<String>)->i32
 {
-    let mut field: HashMap<(i32,i32),i32> = HashMap::new();
-
-    for l in data {
-        let v:Vec<&str> = l.split(" -> ").collect();
-        let p1 = get_point(&v[0].to_string());
-        let p2 = get_point(&v[1].to_string());
-
-        println!("p1 {}x{}",p1.0,p1.1);
-        println!("p2 {}x{}",p2.0,p2.1);
-
-
-        if p1.0==p2.0
-        {
-            for i in i32::min(p1.1,p2.1)..=i32::max(p1.1,p2.1) {
-                let p = (p1.0,i);
-                field.insert(p , field.get(&p).unwrap_or(&0)+1  );
-            }
-        }
-        else if p1.1==p2.1
-        {
-            for i in i32::min(p1.0,p2.0)..=i32::max(p1.0,p2.0) {
-                let p = (i,p1.1);
-                field.insert(p , field.get(&p).unwrap_or(&0)+1  );
-                
-            }
-        }
-        else
-        {
-            if p1.0>p2.0
-            {
-                let t = p1;
-                let p1 = p2;
-                let p2 = t;
-                let mut y = p1.1;
-                for i in p1.0..=p2.0
-                {
-                    let p = (i,y);
-                    field.insert(p , field.get(&p).unwrap_or(&0)+1  );
-                    if p2.1>p1.1 { y+=1; }
-                            else { y-=1; }
-                }
-            }
-            else {
-                let mut y = p1.1;
-                for i in p1.0..=p2.0
-                {
-                    let p = (i,y);
-                    field.insert(p , field.get(&p).unwrap_or(&0)+1  );
-                    if p2.1>p1.1 { y+=1; }
-                            else { y-=1; }
-                }
-                
-            }
-            //}
-            //else {
-              //  
-            //}
-        }
-        
-    }
-    field.values().filter(|&&v|v>1).count() as i32
+    draw(data,true)
 }
 
 #[allow(unused)]
@@ -144,6 +112,7 @@ fn test2()
         "0,9 -> 2,9".to_string(),
         "3,4 -> 1,4".to_string(),
         "0,0 -> 8,8".to_string(),
-        "5,5 -> 8,2".to_string(),    ];
+        "5,5 -> 8,2".to_string(),   
+    ];
     assert_eq!(part2(&v),12);
 }
